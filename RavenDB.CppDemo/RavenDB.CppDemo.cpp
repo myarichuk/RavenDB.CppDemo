373ccf776b767b77598e5d731ed444d0724da4dc
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-11-14 16:49:31 +0200

    initial changes + project config

---
+++ RavenDB.CppDemo/RavenDB.CppDemo.cpp
@@ -0,0 +1,14 @@
+﻿// RavenDB.CppDemo.cpp : Defines the entry point for the application.
+//
+
+#include "RavenDB.CppDemo.h"
+#include <raven_cpp_client.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello RavenDB!" << endl;
+
+
+	return 0;
+}
