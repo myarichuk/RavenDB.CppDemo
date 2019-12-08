95ed62a6eb2d3ebd0a5ac47e309c7849162435e9
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-12-08 11:50:52 +0200

    more explanations in comments, more complex code to highlight client capabilities

--- RavenDB.CppDemo/RavenDB.CppDemo.cpp
+++ RavenDB.CppDemo/RavenDB.CppDemo.cpp
@@ -6,32 +6,56 @@
 using namespace std;
 using namespace ravendb::client::serverwide::operations;
 
-struct User
+struct contact_info
+{
+	string country;
+	string city;
+	string address;
+
+	vector<string> emails;
+};
+
+inline void to_json(nlohmann::json& j, const contact_info& c)
+{
+	using ravendb::client::impl::utils::json_utils::set_val_to_json;
+	set_val_to_json(j, "city", c.city);
+	set_val_to_json(j, "country", c.country);
+	set_val_to_json(j, "address", c.address);
+	set_val_to_json(j, "emails", c.emails);
+}
+
+inline void from_json(const nlohmann::json& j, contact_info& c)
+{
+	using ravendb::client::impl::utils::json_utils::get_val_from_json;
+	
+	get_val_from_json(j, "city", c.city);
+	get_val_from_json(j, "country", c.country);
+	get_val_from_json(j, "address", c.address);
+	get_val_from_json(j, "emails", c.emails);
+}
+
+struct user
 {
 	string id;
 	string name;
 	int age{};
-
-	friend bool operator==(const User& lhs, const User& rhs)
-	{
-		return lhs.name == rhs.name
-			&& lhs.id == rhs.id
-			&& lhs.age == rhs.age;
-	}
+	contact_info contact_info;
 };
 
-inline void to_json(nlohmann::json& j, const User& u)
+inline void to_json(nlohmann::json& j, const user& u)
 {
 	using ravendb::client::impl::utils::json_utils::set_val_to_json;
 	set_val_to_json(j, "name", u.name);
 	set_val_to_json(j, "age", u.age);
+	set_val_to_json(j, "contact_info", u.contact_info);
 }
 
-inline void from_json(const nlohmann::json& j, User& u)
+inline void from_json(const nlohmann::json& j, user& u)
 {
 	using ravendb::client::impl::utils::json_utils::get_val_from_json;
 	get_val_from_json(j, "name", u.name);
 	get_val_from_json(j, "age", u.age);
+	get_val_from_json(j, "contact_info", u.contact_info);
 }
 
 
@@ -40,7 +64,7 @@ int main()
 {
 	cout << "Hello RavenDB!" << endl;
 
-	REGISTER_ID_PROPERTY_FOR(User, id);
+	REGISTER_ID_PROPERTY_FOR(user, id);
 
 	/*
 	Initialize an object that represents a document store.
@@ -56,7 +80,7 @@ int main()
 	Ideally there should be more than one urls here - in this way the initialization would be more resilient since the 
 	client API will try to connect for multiple nodes
 	*/
-	store->set_urls({ "http://localhost:8080" }); //assuming there is unsecure RavenDB instance at this url
+	store->set_urls({ "http://live-test.ravendb.net" }); //assuming there is unsecure RavenDB instance at this url
 
 	/*
 	Set the default database to connect to. 
@@ -84,16 +108,32 @@ int main()
 {
 	//opening a session encapsulates transaction
     auto session = store->open_session("TestDB"); 
-    auto user = std::make_shared<User>();
-    user->name = "John Dow";
-    user->age = 35;
-    session.store(user);
+    auto user_record = std::make_shared<user>();
+    user_record->name = "John Dow";
+    user_record->age = 35;
+	user_record->contact_info = 
+	contact_info
+		{
+			"Dreamland",
+			"Fairy City",
+			"Sesame str. 123/456",
+			{"foo@bar.com","non_existing@mail_domain.com","bar@foo.com"}
+		};
+    session.store(user_record);
 
-    auto user2 = std::make_shared<User>();
-    user2->name = "Jane Dow";
-    user2->age = 24;
-    session.store(user2);
-
+    auto user_record2 = std::make_shared<user>();
+    user_record2->name = "Jane Dow";
+    user_record2->age = 24;
+	user_record2->contact_info = 
+	contact_info
+		{
+			"Jotunheimr",
+			"Jotunfjord",
+			"Frost str. 45/67",
+			{"abc@foobar.com", "jane.dow@foobar.com"}
+		};
+    session.store(user_record2);
+	
 	session.save_changes(); //tx commit, we talk with server here
 }
 
@@ -102,19 +142,20 @@ int main()
 		auto session = store->open_session();
 
 		//fetch record with id = 'users/1-A'
-		const auto john = *session.load<User>("users/1-A");
+		const auto john = *session.load<user>("users/1-A");
 
 		cout<< john.name << "'s age is " << john.age << endl;
 
 		//execute query and fetch all users that match filtering criteria with sorting
-		const auto usersNamedJane = session.query<User>()
-											->where_starts_with("name","Jane")
-											->order_by_descending("name")
-											->to_list();
-
+		const std::vector<std::shared_ptr<user>> usersNamedJane = session.query<user>()
+		                                                                 ->where_starts_with("name", "Jane")
+		                                                                 ->order_by_descending("name")
+		                                                                 ->to_list();		
 		//should be always one user
 		cout << "found " << usersNamedJane.size() << " user(s) with name 'Jane'" << endl;
-		cout<< usersNamedJane[0]->name << "'s age is " << usersNamedJane[0]->age << endl;		
+		cout<< usersNamedJane[0]->name << "'s age is " << usersNamedJane[0]->age << endl;
+		for(const auto& email : usersNamedJane[0]->contact_info.emails)
+			cout << "email:" << email << endl;
 	}
 
 	//delete the database on the server
