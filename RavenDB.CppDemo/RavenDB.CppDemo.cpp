9c16fdc0fabfa91b239c4bd72ab8c91274dcd6a8
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-12-10 13:43:10 +0200

    adjust demo code to use parameters instead of hard coded filter values

--- RavenDB.CppDemo/RavenDB.CppDemo.cpp
+++ RavenDB.CppDemo/RavenDB.CppDemo.cpp
@@ -3,6 +3,7 @@
 
 #include "RavenDB.CppDemo.h"
 #include <algorithm>
+#include <array>
 using namespace std;
 using namespace ravendb::client::serverwide::operations;
 
@@ -31,13 +32,29 @@ inline void from_json(const nlohmann::json& j, user& u)
 	get_val_from_json(j, "emails", u.emails);
 }
 
+struct email_count_result
+{
+	string email;
+	int count{};
+};
 
+inline void to_json(nlohmann::json& j, const email_count_result& u)
+{
+	using ravendb::client::impl::utils::json_utils::set_val_to_json;
+	set_val_to_json(j, "email", u.email);
+	set_val_to_json(j, "count", u.count);
+}
+
+inline void from_json(const nlohmann::json& j, email_count_result& u)
+{
+	using ravendb::client::impl::utils::json_utils::get_val_from_json;
+	get_val_from_json(j, "email", u.email);
+	get_val_from_json(j, "count", u.count);
+}
 
 int main()
 {
-	cout << "Hello RavenDB!" << endl;
-	cout << "============" <<endl;
-
+	//let RavenDB client know what is the 'id' of the document class
 	REGISTER_ID_PROPERTY_FOR(user, id);
 
 	/*
@@ -69,24 +86,27 @@ int main()
 	//notice 'store' being an entry point for an operation
 	const auto databaseNames = *store->maintenance()->server()->send(GetDatabaseNamesOperation(0, INT_MAX));
 
-	//create TestDB if not exists
-	if(find(databaseNames.begin(), databaseNames.end(), "TestDB") == databaseNames.end())
+	//if TestDB exists, delete it to have "fresh" database for demo
+	if(find(databaseNames.begin(), databaseNames.end(), "TestDB") != databaseNames.end())
 	{
-		auto dr = ravendb::client::serverwide::DatabaseRecord();
-		dr.database_name = "TestDB";
-
-		//executing this creates a database, if it already exists, throws
-		store->maintenance()->server()->send(CreateDatabaseOperation(dr));
-		cout << "created database 'TestDB'" <<endl;
+		//delete the database on the server
+		store->maintenance()->server()->send(DeleteDatabasesOperation("TestDB", true));
+		cout << "deleted database 'TestDB'" <<endl;
 	}
 
+	auto dr = ravendb::client::serverwide::DatabaseRecord();
+	dr.database_name = "TestDB";
+	//executing this creates a database, if it already exists, throws
+	store->maintenance()->server()->send(CreateDatabaseOperation(dr));
+	cout << "created database 'TestDB'" <<endl;
+	
 {
 	//opening a session encapsulates transaction
     auto session = store->open_session(); 
     auto user_record = std::make_shared<user>();
     user_record->name = "John Doe";
     user_record->age = 35;
-	user_record->emails = {"john.doe@example.com","non_existing@example.com","bar@example.com"};
+	user_record->emails = {"john.doe@example.com","abc@example.com","bar@example.com"};
     session.store(user_record); //this doesn't do roundtrip to the server
 
     auto user_record2 = std::make_shared<user>();
@@ -98,7 +118,7 @@ int main()
 	auto user_record3 = std::make_shared<user>();
     user_record3->name = "Jack Foobar";
     user_record3->age = 28;
-	user_record3->emails = {"jack.foobar@example.com", "xyz@example.com"};
+	user_record3->emails = {"jack.foobar@example.com", "abc@example.com", "bar@example.com"};
     session.store(user_record3); 
 	
 	session.save_changes(); //tx commit, we talk with server here
@@ -118,14 +138,66 @@ int main()
 		cout << "load by id results" <<endl;
 		cout<< john.name << "'s age is " << john.age << endl;
 
+		cout << "============" <<endl;
+		cout << "raw query #1 results" <<endl;
+
+		//full-text search on 'name' field
+		const std::vector<std::shared_ptr<user>> rawUsersNamedJane = 
+			session.advanced().raw_query<user>("from users where search(name, $name_to_search)")
+							  ->add_parameter("name_to_search","john")
+							  ->to_list();
+		for(const auto& user : rawUsersNamedJane)
+			cout << "name:" << user->name << endl;
+
+		cout << "============" <<endl;
+		cout << "raw query #2 results" <<endl;
+		
+		
+		const char *aggregation_query = 
+		   "from users "
+		   "group by emails[] "
+		   "order by count() desc "
+		   "select key() as email, count() as count";
+
+		const auto rawCountEmailsPerUser = 
+			session.advanced().raw_query<email_count_result>
+				(aggregation_query)->to_list();
+
+		for(const auto& result : rawCountEmailsPerUser)
+			cout << "email:" << result->email << ", count: " << result->count << endl;
+
+		//full-text search on 'name' field
+		const vector<shared_ptr<user>> rawCountUsersPerEmail = session.advanced().raw_query<user>("from users where search(name, 'john')")->to_list();
+		for(const auto& user : rawUsersNamedJane)
+			cout << "name:" << user->name << endl;
+
+		cout << "============" <<endl;
+		cout << "raw query #3 results" <<endl;
+
+		std::string complex_query = 
+		   "from users "
+		   "where emails[] in ( $email_list ) or"
+		   "(age > 25 and endsWith(name, 'Doe'))";
+
+		const vector<shared_ptr<user>> complexQueryResult = 
+			session.advanced().raw_query<user>(complex_query)
+			       ->add_parameter("email_list",
+			           nlohmann::json::array(
+						   {"john.doe@example.com","jane.doe@example.com" }))
+		           ->to_list();
+
+		for(const auto& user : complexQueryResult)
+			cout << "name:" << user->name << endl;
+
+		cout << "============" <<endl;
 		//execute query and fetch all users that match filtering criteria with sorting
 		const std::vector<std::shared_ptr<user>> usersNamedJane = session.query<user>()
 		                                                                 ->where_starts_with("name", "Jane")
 		                                                                 ->order_by_descending("name")
 		                                                                 ->to_list();
 
-		cout << "============" <<endl;
-		cout << "query #1 results" <<endl;
+		cout << "============" << endl;
+		cout << "query #1 results" << endl;
 		cout << "found " << usersNamedJane.size() << " user(s) with name 'Jane'" << endl;
 		cout<< usersNamedJane[0]->name << "'s age is " << usersNamedJane[0]->age << endl;
 		for(const auto& email : usersNamedJane[0]->emails)
@@ -138,7 +210,7 @@ int main()
 											 {
                                                  "john.doe@example.com", 
                                                  "jane.doe@example.com"
-											 }, false)
+											 })
 		                                 ->to_list();		
 
 		cout << "============" <<endl;
@@ -162,7 +234,7 @@ int main()
 			  {
                 "john.doe@example.com", 
                 "jane.doe@example.com"
-              }, false)
+              })
 		    ->or_else() // OR relationship to other clauses
 			->open_subclause() //open parenthesis
 			  ->where_greater_than("age", 25)
@@ -180,10 +252,6 @@ int main()
 			cout << "name:" << user->name << endl;
 		cout << "============" <<endl;
 	}
-
-	//delete the database on the server
-	store->maintenance()->server()->send(DeleteDatabasesOperation("TestDB", true));
-	cout << "deleted database 'TestDB'" <<endl;
 
  	return 0;
 }
